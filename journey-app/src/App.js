c04666f413cbac304ffe8f329d2d9394a88add58
mickey-40 <mickeyarnold53@yahoo.com>
2022-09-21 18:52:49 -0500

    added react-bootstrap and started class in App.js

--- journey-app/src/App.js
+++ journey-app/src/App.js
@@ -1,12 +1,31 @@
-
+import React, { Component } from 'react'
 import './App.css';
+// import {Button, Alert, Card} from 'react-bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
+let baseURL =''
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'heroku backend url here'
+}
+
+console.log('current base URL: ', baseURL)
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      liveEvents: []
+    }
+  }
+  render (){
   return (
     <div>
       <h1>Journey-App</h1>
     </div>
   );
 }
-
+}
 export default App;
