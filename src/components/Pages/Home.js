cf0148ea3613d35ec564d7f9e0f039f352856f81
jasonaul <jason.aul@gmail.com>
2022-10-06 22:56:45 -0400

    home page styling

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -15,7 +15,7 @@ import '../../App.css';
         </div>
       
         </div>
-        <div class="inspiration"><hr></hr>Find Event Inspiration</div>
+        <div class="inspiration"><hr></hr><h1 id="inspiroText">Find Event Inspiration</h1></div>
             <div class="inspirationImg">
            <a href="https://www.eventbrite.com/"> <img class="eventbrite" id="inspiroImg"  src="https://cdn.evbstatic.com/s3-build/731483-rc2022-10-05_16.04-d21b382/django/images/logos/eb_orange_on_white_1200x630.png"></img></a>
            <a href="https://www.facebook.com/events/"><img id ="inspiroImg" src="https://logos-world.net/wp-content/uploads/2020/04/Facebook-Logo-2019-present.jpg"></img></a>
